c5c0b04f64c256416387b59403724bf99047065d
Harihar Nautiyal <c@harihar.site>
2025-09-08 21:16:12 +0530

    Pub

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+    return (
+        <main className="flex flex-col justify-center items-center flex-1 p-4 text-center">
+            <h1 className="text-4xl font-bold mb-4">Welcome to endie</h1>
+            <p className="text-lg text-gray-700">
+                Endie is social media platform for indie developers.
+            </p>
+        </main>
+    )
+}
